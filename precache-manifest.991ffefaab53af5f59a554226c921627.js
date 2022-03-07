352955050893beb1b4ef437a38f9a2384517d64d
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-03-07 12:09:30 -0500

    Updates

---
+++ precache-manifest.991ffefaab53af5f59a554226c921627.js
@@ -0,0 +1,62 @@
+self.__precacheManifest = [
+  {
+    "revision": "0452535bb9b918b71260",
+    "url": "/photo-port/static/css/main.7ded1439.chunk.css"
+  },
+  {
+    "revision": "0452535bb9b918b71260",
+    "url": "/photo-port/static/js/main.610ee757.chunk.js"
+  },
+  {
+    "revision": "80e4efe8fb8f90acaed2",
+    "url": "/photo-port/static/js/runtime~main.d02bdb6d.js"
+  },
+  {
+    "revision": "56770eacdf687aeac0c4",
+    "url": "/photo-port/static/js/2.283e84af.chunk.js"
+  },
+  {
+    "revision": "0222b4ed3fafae3e17ab",
+    "url": "/photo-port/static/js/3.50b2d2e5.chunk.js"
+  },
+  {
+    "revision": "70af7d8942a137723a9ec6998fb98785",
+    "url": "/photo-port/static/media/self-img.70af7d89.JPG"
+  },
+  {
+    "revision": "0c6c5ac8d1bef70ab32546c0333ced29",
+    "url": "/photo-port/static/media/portfolioResume.0c6c5ac8.pdf"
+  },
+  {
+    "revision": "720f48773335a38b7d2a8f97ade72b8f",
+    "url": "/photo-port/static/media/background.720f4877.jpg"
+  },
+  {
+    "revision": "00d0ae9033c5f1c968280336703f03ce",
+    "url": "/photo-port/static/media/0.00d0ae90.png"
+  },
+  {
+    "revision": "80ffcd83f51220bd8ef60cb2d1af9934",
+    "url": "/photo-port/static/media/1.80ffcd83.png"
+  },
+  {
+    "revision": "6805fadf0ff712b6383eb5c30195300c",
+    "url": "/photo-port/static/media/2.6805fadf.png"
+  },
+  {
+    "revision": "adfc983bf8ed10a0dadde1e063e0d364",
+    "url": "/photo-port/static/media/3.adfc983b.png"
+  },
+  {
+    "revision": "eb44e5e1d9ce563b13719ba5d2b7449d",
+    "url": "/photo-port/static/media/4.eb44e5e1.png"
+  },
+  {
+    "revision": "adec1363e20cb9a4e262dff4dd98ef1d",
+    "url": "/photo-port/static/media/5.adec1363.png"
+  },
+  {
+    "revision": "09b5c7bae8b7cc1f066d0df3d7219f56",
+    "url": "/photo-port/index.html"
+  }
+];
