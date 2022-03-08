2644b95ff404ec2265600ab3140e06cab8d43e91
CmcCormack92 <86354138+CmcCormack92@users.noreply.github.com>
2022-03-08 14:34:24 -0500

    add about me

--- src/components/About/index.js
+++ src/components/About/index.js
@@ -9,7 +9,14 @@ function About() {
             </div>
             <div className='p-wrap'>
             <p>
-            Lorem ipsum, dolor sit amet consectetur adipisicing elit. Nulla alias est maxime voluptatem commodi esse temporibus autem repellat quia adipisci cupiditate labore possimus quos repudiandae voluptate, dolor ipsa itaque? Porro!
+             Hello! My name is Chris, I am a full stack developer located in the Greater Tampa Bay Area.
+             <br/>
+             <br/>
+             Wanting to change my career, I thought it would be impossible to find something that i truely enjoyed. That is until I wrote 'Hello World!', from there it all changed.
+             <br/>
+             <br/>
+             Specializing in the MERN stack, I have built many different applications on both the front and back end. Please visit my GitHub using the link on the bottom of the page to view them all!
+             
             </p>
             </div>
             
